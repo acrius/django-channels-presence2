e5d19206d514dad343078dc202a5d18ab8d67d1c
acrus <acrius@mail.ru>
2018-03-11 15:49:34 +0700

    Fix setup file.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-from distutils.core import setup
 
-from .channels_presence2 import __version__
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+from channels_presence2 import __version__ as version
 
 setup(
-	name='django-channels-presence2',
-	packages=['channel_presence2'],
-	version=__version__,
-	description='Simple presence boilerplate for django-channels 2 and channel redis backend.',
-	author='acrius',
-	author_email='acrius@mail.ru',
-	url='https://github.com/acrius/django-channels-presence2',
-	keywords=['django', 'channels', 'presence'],  # arbitrary keywords
-	classifiers=[],
+    name='django-channels-presence2',
+    version=version,
+    packages=find_packages(),
+    author='acrius',
+    author_email='acrius@mail.ru',
+    url='https://github.com/acrius/django-channels-presence2',
+    description='Simple presence boilerplate for django-channels 2 and channel redis backend.',
+    license='MIT',
+    keywords='',
+    long_description=open(join(dirname(__file__), 'README.MD')).read(),
+    include_package_data=True,
+    include_packages_data=True,
+    install_requires=[]
 )
